9c4b7e01d2830a875a9cd4d977f3d51ed82ec269
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-15 14:38:05 +0200

    End

--- source/js/index.js
+++ source/js/index.js
@@ -12,5 +12,39 @@ const demainDate = () => {
 
     let jour = new Date(aujourdhui);
     jour.setDate(jour.getDate() + 1);
-    
+
+    let demain = jour.toISOString().split('T')[0];
+    end.value = demain;
+    end.min = demain;
 }
+
+demainDate()
+
+start.addEventListener('change', (e) => {
+
+    let jour = new Date(e.target.value);
+    jour.setDate(jour.getDate() + 1);
+    let demain = jour.toISOString().split('T')[0];
+    end.min = demain;
+    end.value = demain;
+
+})
+
+const TOTAL = () => {
+
+    let date01 = new Date(start.value);
+    let date02 = new Date(end.value);
+
+    let difference = Math.abs(date02 - date01);
+
+    let diffJours = Math.ceil(difference / (1000 * 60 * 60 * 24))
+    let nightPrice = document.getElementById('nightPrice').innerHTML;
+
+    let total = diffJours * nightPrice;
+    document.getElementById('total').innerHTML = total;
+}
+
+start.addEventListener('change', () => TOTAL());
+end.addEventListener('change', () => TOTAL());
+
+TOTAL()
